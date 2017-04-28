e9cfaded86ed6e38d8fdbde243a572d5704102dc
gigarthan <gigajpx@gmail.com>
2017-04-28 19:30:28 +0530

    creating main

---
+++ src/inventory/Main.java
@@ -0,0 +1,39 @@
+package inventory;/**
+ * Created by gigar on 28-Apr-17.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private Stage primaryStage;
+    private BorderPane mainLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Inventory Control System");
+        showMainView();
+
+    }
+
+    private void showMainView() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+}
